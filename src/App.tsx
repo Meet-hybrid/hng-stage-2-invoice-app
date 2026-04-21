ef7eafafcca2b94349d9cbc2c4467900603ea53d
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-04-21 11:02:06 +0100

    chore: set up Tailwind CSS and clean starter template

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-white text-black">
+      <h1 className="p-8 text-3xl font-bold">Invoice App</h1>
+    </main>
+  );
+}
+
+export default App;
